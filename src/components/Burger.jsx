368010df7eb6c93d5409b597eafa40576cd4ce6a
met43211 <matvey-anashkin@yandex.ru>
2023-11-27 12:43:29 +0300

    final3

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -8,7 +8,12 @@ function Burger() {
     const [burger, setBurger] = useState(false)
     return ( 
         <>
-            <button className="burger-menu-button" onClick={()=>setBurger(prev=>!prev)}>Меню</button>
+            <button className="burger-menu-button" onClick={()=>setBurger(prev=>!prev)}>
+                <svg width="30" height="9" viewBox="0 0 30 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="-0.000244141" y="0.467285" width="30" height="2" fill="white"/>
+                <rect x="-0.000244141" y="6.53271" width="30" height="2" fill="white"/>
+                </svg>
+            </button>
             {burger&&<>
                 <div className="burger-wrapper" onClick={()=>setBurger(false)}>
                 </div>
